d49bccd3897bff522cce2cf100d7de04dfea4532
sidon <sidoncd@gmail.com>
2017-03-10 09:18:05 -0300

    Retirada do model Profile, desnecessário para a respostas

--- core/forms.py
+++ core/forms.py
@@ -1,8 +1,5 @@
 from django import forms
-from django.conf import settings
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile
 
 
 class UserRegistrationForm(forms.ModelForm):
@@ -10,4 +7,3 @@ class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email')
-
--- core/models.py
+++ core/models.py
@@ -1,7 +1,1 @@
 from django.db import models
-from django.conf import settings
-
-class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
-    email = models.CharField(max_length=150)
-
